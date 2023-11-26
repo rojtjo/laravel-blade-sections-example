3d9190f8beed88504a2b30f001d14175da2f93af
Roj Vroemen <roj@vroe.men>
2023-11-26 14:34:52 +0100

    Adding routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('bar0', 'bar0');
+Route::view('bar1', 'bar1');
+Route::view('bar2', 'bar2');
+Route::view('bar3', 'bar3');
